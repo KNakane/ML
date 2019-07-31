1ce44eb31654279fb929d3b7aa2dae87898b6454
Koki Nakane <k_nakane@mbp02.isl.melco.co.jp>
2019-07-31 10:43:06 +0900

    [update] argparse

--- train.py
+++ train.py
@@ -1,10 +1,15 @@
 import os, sys
+import argparse
 import sklearn
+from methods.boosting import *
 
 
-def main():
-    pass
-
+def main(args):
+    return
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data', default='boston', choices=['boston', 'iris', 'diabetes', 'digits', 'linnerud'])
+    parser.add_argument('r', default='XGBoost', choices=['XGBoost', 'LightGBM'])
+    args = parser.parse_args()
+    main(args)
