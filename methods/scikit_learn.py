6c1de3a41e367b70b2ba0f43c88356dfb6f0e26f
KNakane <koki.bsb@gmail.com>
2019-08-04 19:09:21 +0900

    [update] ridge

--- methods/scikit_learn.py
+++ methods/scikit_learn.py
@@ -5,13 +5,26 @@ import sklearn.svm as sk
 from sklearn import linear_model
 
 class Linear():
-    def __init__(self, kernel='rbf', C=1e3, gamma=0.1):
+    def __init__(self, kernel=None, C=None, gamma=None):
         self.model = linear_model.LinearRegression()
 
     def train(self, x, y):
         self.model = self.model.fit(x, y)
 
-    def predict(self, x, y):
+    def predict(self, x):
+        return self.model.predict(x)
+
+    def loss(self, pred, grand_truth):
+        return np.sqrt(sklearn.metrics.mean_squared_error(grand_truth, pred))
+
+class Ridge():
+    def __init__(self, kernel=None, C=None, gamma=None):
+        self.model = linear_model.Ridge()
+
+    def train(self, x, y):
+        self.model = self.model.fit(x, y)
+
+    def predict(self, x):
         return self.model.predict(x)
 
     def loss(self, pred, grand_truth):
@@ -25,7 +38,7 @@ class SVR():
     def train(self, x, y):
         self.model = self.model.fit(x, y)
 
-    def predict(self, x, y):
+    def predict(self, x):
         return self.model.predict(x)
 
     def loss(self, pred, grand_truth):
